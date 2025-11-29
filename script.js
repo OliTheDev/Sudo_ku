3f4b63c183b3574742205345c9da80ef0517042a
Oliver <olithedev@proton.me>
2025-11-29 12:48:54 -0400

    almost have the hash map system working, tweaking for bugs and optimization

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const board = [
   [3,0,6,0,0,0,0,0,0]
   ]
 
+let empties =  0
 
 function sort(num,axis,array) {
 const xAx = axis
@@ -54,217 +55,25 @@ sort(7,"c",board[8])
 }
 
 
-
-let cube =[
-['00', '01', '02', '03', '04', '05', '06', '07', '08'],
- ['10', '11', '12', '13', '14', '15', '16', '17', '18'],
-['20', '21', '22', '23', '24', '25', '26', '27', '28'],
-['30', '31', '32', '33', '34', '35', '36', '37', '38'],
-['40', '41', '42', '43', '44', '45', '46', '47', '48'],
-['50', '51', '52', '53', '54', '55', '56', '57', '58'],
-['60', '61', '62', '63', '64', '65', '66', '67', '68'],
-['70', '71', '72', '73', '74', '75', '76', '77', '78'],
-['80', '81', '82', '83', '84', '85', '86', '87', '88']]
-
-let rows = {
-  one : ["00","01","02",10,11,12,20,21,22],
-  two : ["03","04","05",13,14,15,23,24,25],
-  three : ["06","07","08",16,17,18,26,27,28],
-  four : [30,31,32,40,41,42,50,51,52],
-  five : [33,34,35,43,44,45,53,54,55],
-  six : [36,37,38,46,47,48,56,57,58],
-  seven : [60,61,62,70,71,72,80,81,82],
-  eight : [63,64,65,73,74,75,83,84,85],
-  nine : [66,67,68,76,77,78,86,87,88]
-}
-
-let columns = {
-  one : ["00","03","06",30,33,36,60,63,66],
-  two : ["01","04","07",31,34,37,61,64,67],
-  three : ["02","05","08",32,35,38,62,65,68],
-  four : [10,13,16,40,43,46,70,73,76],
-  five : [11,14,17,42,44,47,71,74,77],
-  six : [12,15,18,42,45,48,72,75,78],
-  seven : [20,23,26,50,53,56,80,83,86],
-  eight : [21,24,27,51,54,57,81,84,87],
-  nine : [22,25,28,52,55,58,82,85,88]
-  }
-
-function isWin(){
-let empties = []
-cube.forEach((chk) => {
-chk.forEach((cell) => {
-if(document.getElementById(cell).innerHTML == 0){
-empties.push(cell)
-}
-})
-})
-if(empties.length == 0 ){console.log("you win") }else{
-console.log(empties.length, "spaces yet to fill")}
-}
- 
-function bigIf(x){
-  if(x < 30){
-    for(let i = 0; i < 9; i++){
-      if(rows.one[i] == x){        
-        return {
-          thisRow: rows.one,
-          index : i
-          }
-        }
-      if(rows.two[i] == x){    
-       return {
-          thisRow: rows.two,
-          index : i
-          }
-        }
-     if(rows.three[i] == x){
-       return {
-          thisRow: rows.three,
-          index : i
-          }
-       
-        }
-     } 
-    } else if(x < 60){
-      for(let i = 0; i < 9; i++){
-
-      if(rows.four[i] == x){        
-        return {
-          thisRow: rows.four,
-          index : i
-          }
-        }
-
-      if(rows.five[i] == x){    
-       return {
-          thisRow: rows.five,
-          index : i
-          }
-        }
-
-     if(rows.six[i] == x){
-       return {
-          thisRow: rows.six,
-          index : i
-          }
-        }
-        
-      }
-     }
-      else{
-      
-        for(let i = 0; i < 9; i++){
-      if(rows.seven[i] == x){        
-        return {
-          thisRow: rows.seven,
-          index : i
-          }
-        }
-      if(rows.eight[i] == x){    
-       return {
-          thisRow: rows.eight,
-          index : i
-          }
-        }
-     if(rows.nine[i] == x){
-       return {
-          thisRow: rows.nine,
-          index : i
-          }
-       
-        }
-        
-       } 
-        
-        }
-  }
-  
-function smallIf(x){
-  switch(x){
-      case 0 : return columns.one
-      break;
-      case 1 : return columns.two
-      break;
-      case 2: return columns.three
-      break;
-      case 3 : return columns.four
-      break;
-      case 4 : return columns.five
-      break;
-      case 5: return columns.six
-      break;
-      case 6 : return columns.seven
-      break;
-      case 7 : return columns.eight
-      break;
-      case 8 : return columns.nine
-      break;
-      }
-  }
-
-function mainAlgo(thing,x,y,z){
-  
-  //check row first 
-  for(let i = 0; i < x.length; i++){
-    if(document.getElementById(x[i]).innerHTML != "0"){
-       if(document.getElementById(x[i]).innerHTML == thing)
-      {
-        return false 
-      }
-    }
-    }
-  
- //then check column
-  
-  for(let i = 0; i < y.length; i++){
-    if(document.getElementById(y[i]).innerHTML != "0"){
-       if(document.getElementById(y[i]).innerHTML == thing)
-      {
-        return false 
-      }
-    }
- }
-  
-  // check box
-   
-  let Square = cube[z]
-  for(let i = 0; i < Square.length; i++){
-    if(document.getElementById(Square[i]).innerHTML != "0"){
-       if(document.getElementById(Square[i]).innerHTML == thing)
-      {
-        return false 
-      }
-
-    }
-
- }
- return true   
-  }
-
 function rowchecker(row,num,input){
 let col = 'd'
 let numb = num
 for(let i = 0;i<9;i++){
-	if(i>2){
-	col = "e"
-  	}
-		if(i == 3){
-		numb = num}
-			if(i>5){
-			col = "f" }
-if(i == 6){
-numb = num
-}
+	if(i > 2){col = "e"}
+		if(i == 3){numb = num}
+			if(i>5){col = "f" }
+if(i == 6){numb = num}
 let newname = row + col + numb 	
 if(map.get(newname) == input){
-return false
+
 refresh()
+return false
+
 }
 numb = numb + 1
-return true
-}
 
+}
+return true
 }
 
 function colchecker(arrnum,current,input){
@@ -295,8 +104,11 @@ refresh()
 console.log("absolutly not")
 return false
 }
+}
+
 return true
-}}
+
+}
 
 
 function isAble(input,x){
@@ -307,7 +119,6 @@ const box = brakedown[0] + brakedown[1]
 for(let i = 1;i < 10;i++){
 let searchbox = box + i
 if(map.get(searchbox) == input){
-console.log("not in this box")
 refresh()
 return
 } }
@@ -315,33 +126,29 @@ return
 //check row
 if(brakedown[2] <4){
 if(rowchecker(brakedown[0],1,input) == false){
-console.log("not in this row")  
 return
   }
  }
 
 else if(brakedown[2] > 3 && brakedown[2] < 7){
 if(rowchecker(brakedown[0],4,input) == false){
-
-console.log("not in this row")
 return 
 }
 }
 else if(brakedown[2] > 6){
 
 if(rowchecker(brakedown[0],7,input) == false){
-console.log("not in this row")
 return 
 }
 }
 
 //check column
 if(brakedown[2] == 1 ||brakedown[2] == 4 ||brakedown[2] == 7){
-console.log("first")
+
 let arrnum = [1,4,7] 
 
 if(colchecker(arrnum,brakedown[1],input) == false){
-console.log("not in this column")
+refresh()
 return
 }
 
@@ -349,57 +156,33 @@ return
 
 
 if(brakedown[2] == 2 ||brakedown[2] == 5 ||brakedown[2] == 8){
-console.log("second")
 let arrnum = [2,5,8]
 if(colchecker(arrnum,brakedown[1],input) == false){
-console.log("not in this column")
+refresh()
 return
 }
 }
 
 
 if(brakedown[2] == 3 ||brakedown[2] == 6 ||brakedown[2] == 9){
-console.log("third")
 let arrnum = [3,6,9]
 if(colchecker(arrnum,brakedown[1],input) == false){
-console.log("not in this column")
+refresh()
 return
 }
 }
 
-console.log("does this happen")
 let thething = document.getElementById(x)
 map.set(x, input)
 thething.innerHTML = input
 thething.classList.remove('empty')
 thething.classList.add('filled')
-console.log(map.get(x))
+empties = empties - 1
+console.log(empties)
 
-/* 
-let thing = el.innerHTML
-                 
-                      const theplace = document.getElementById(sel)
-      if(mainAlgo(thing,x,y,z) == true) {                     
-theplace.classList.add('filled');
-theplace.classList.remove('selected')
-theplace.classList.remove('selectedFull')
-theplace.classList.remove('cell')
-if(theplace.innerHTML == 0){
-theplace.classList.remove('empty')      
-theplace.innerHTML = thing   
-//console.log(theplace)
-          }}
- else{
-           
-
-theplace.classList.remove('selected');
-theplace.classList.remove('selectedFull');
-if(theplace.innerHTML == 0) {
-    theplace.classList.add('empty')
+if(empties == 0){
+console.log("you win")
 }
-          
-                         }        
-  */
   refresh()
 
 
@@ -409,7 +192,7 @@ if(theplace.innerHTML == 0) {
 function refresh(x) {
   let butlist = document.querySelectorAll('.button');
   butlist.forEach((el) => {el.classList.remove('button');
-
+//el.removeEventListener("click", clicker)
                   el.classList.add('buttonDis')}) 
 let old = document.querySelectorAll('.selected')
 old.forEach((obj) => {
@@ -421,45 +204,27 @@ old2.forEach((obj) => {
 obj.classList.remove('selectedFull')
 })  
 
-/*const theplace = document.getElementById(x)
-const del = document.getElementById("delete")
-let refdel = del.cloneNode(true);
-del.remove()
-document.getElementById("otherbutts").appendChild(refdel)
-refdel.addEventListener("click", function clicked(stuff) {
-theplace.innerHTML = 0
-theplace.classList.remove('selected')
-theplace.classList.remove('selectedFull')
-theplace.classList.add('empty')
-
-let butlist = document.querySelectorAll('.button')  
-
-butlist.forEach((el) => {
-  
-    el.classList.remove('button');
-      el.classList.add('buttonDis') 
-      let newthing = el.cloneNode(true); 
-
-    el.remove()
-
-    document.getElementById("nums").appendChild(newthing)
-                 
-                        } )
-
-}, {once:true})
-*/
-
-
-
 
 }
 
   function select(x) {
-if(document.getElementById(x).classList.contains("placed")){
+let elem = document.getElementById(x)
+refresh()
+if(elem.classList.contains("placed")){
 refresh()
 return
 }
-//clear all old selected cells
+
+if(elem.classList.contains("filled")){
+let  delbut = document.getElementById("delete")
+delbut.classList.remove("deletedis")
+delbut.classList.add("delete")
+delbut.addEventListener("click", function getrid(){map.set(x,0); elem.innerHTML = 0; refresh(); elem.classList.add("empty")  },{once:true})
+}
+
+//clear all old selected cells . this is not a duplacit of the refresh function and has specific use case in preventing the phantom select bug.
+
+/*
 let old = document.querySelectorAll('.selected')
 old.forEach((obj) => {
 obj.classList.remove('selected')
@@ -469,11 +234,11 @@ let old2 = document.querySelectorAll('.selectedFull')
 old2.forEach((obj) => {
 obj.classList.remove('selectedFull')
 })
+
+*/ 
 //assign new selected cell
 
 let value = map.get(x)
-console.log(value)
-
 
 if(value == 0){
 document.getElementById(x).classList.add('selected')
@@ -481,13 +246,6 @@ document.getElementById(x).classList.add('selected')
 document.getElementById(x).classList.add('selectedFull')
 }
   
-/* 
-let {thisRow, index}= bigIf(x)
-  let thisCol = smallIf(index)
-  let thisBox = document.getElementById(x).parentElement.id
-  let boxSplitter = thisBox.split("")
-let boxNum = Number(boxSplitter[1])
- */
 
 
 //set up number select buttons
@@ -516,10 +274,12 @@ for(const [key,value] of map.entries()) {
 let thing = document.getElementById(key)
 thing.innerHTML = value
 if(value > 0){thing.classList.add('placed')}
-else{thing.classList.add('empty')}
+else{thing.classList.add('empty')
+empties++
+}
 addListen(thing)
 }
-  
+  console.log(empties)
 }
     
   
